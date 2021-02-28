7ff213f1e8d7a094eb185a0c60dac120a481d964
jessica you <jessicaxytan@gmail.com>
2021-02-27 21:30:27 -0500

    commit tree 5e2150c68075f17714b03b7bb5d92440e4a6d295

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,19 @@
-def my_each # put argument(s) here
-  # code here
+require 'pry'
+
+
+def my_each(words) # words is the array argument
+  
+  #"while" #prints while without calling on item array
+  count = 0
+
+  while count < words.size
+
+    yield words[count]
+    count += 1
+
+  end
+
+  return words
+
 end
+
